b22e703696f0d1ec44e11090a472b27573abaa79
murat-avci <avcmurat@yahoo.com>
2020-09-23 20:40:29 +0300

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,52 +1,44 @@
 'use strict';
 
-const WIZARD_NAME = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const WIZARD_FIRST_NAMES = [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const WIZARD_LAST_NAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const EYE_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const WIZARD_COUNT = 4;
 
-const WIZARD_SURNAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const userDialog = document.querySelector(`.setup`);
+const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
+    .content
+    .querySelector(`.setup-similar-item`);
+const similarListElements = userDialog.querySelector(`.setup-similar-list`);
 
-const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-
-const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
-
-let userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
 
-let similarListElement = userDialog.querySelector(`.setup-similar-list`);
-
-let similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
-  .content
-  .querySelector(`.setup-similar-item`);
-
-let getRandomElement = function (arr) {
-  let rand = Math.floor(Math.random() * arr.length);
-
-  return arr[rand];
+const generateRandomIndex = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-let wizards = [
-  {
-    name: getRandomElement(WIZARD_NAME) + ` ` + getRandomElement(WIZARD_SURNAME),
-    coatColor: getRandomElement(COAT_COLOR),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(WIZARD_NAME) + ` ` + getRandomElement(WIZARD_SURNAME),
-    coatColor: getRandomElement(COAT_COLOR),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(WIZARD_NAME) + ` ` + getRandomElement(WIZARD_SURNAME),
-    coatColor: getRandomElement(COAT_COLOR),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(WIZARD_NAME) + ` ` + getRandomElement(WIZARD_SURNAME),
-    coatColor: getRandomElement(COAT_COLOR),
-    eyesColor: getRandomElement(EYES_COLOR)
-  }
-];
+const getRandomElement = function (arr) {
+  return arr[generateRandomIndex(0, arr.length)];
+};
 
-let renderWizard = function (wizard) {
+const generateWizards = function () {
+  let wizards = [];
+
+  for (let i = 0; i < WIZARD_COUNT; i++) {
+    let wizard = {
+      name: `${getRandomElement(WIZARD_FIRST_NAMES)} ${getRandomElement(WIZARD_LAST_NAMES)}`,
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYE_COLORS)
+    };
+    wizards.push(wizard);
+  }
+  return wizards;
+};
+
+const renderOneWizard = function (wizard) {
   let wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
@@ -56,11 +48,17 @@ let renderWizard = function (wizard) {
   return wizardElement;
 };
 
-let fragment = document.createDocumentFragment();
-for (let i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+const renderWizards = function (arrWizards) {
+  let fragment = document.createDocumentFragment();
 
-similarListElement.appendChild(fragment);
+  for (let i = 0; i < arrWizards.length; i++) {
+    fragment.appendChild(renderOneWizard(arrWizards[i]));
+  }
+  return fragment;
+};
+
+const wizards = generateWizards();
+
+similarListElements.appendChild(renderWizards(wizards));
 
 userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
